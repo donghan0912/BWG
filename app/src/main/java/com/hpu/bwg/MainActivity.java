bfeedb23b7a3b6a18e88e37f51ade686feb65292
董晗 <donghan2010@126.com>
2017-11-18 14:08:40 +0800

    add submodule

--- app/src/main/java/com/hpu/bwg/MainActivity.java
+++ app/src/main/java/com/hpu/bwg/MainActivity.java
@@ -3,11 +3,14 @@ package com.hpu.bwg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hpu.common.TestUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TestUtils.t();
     }
 }
